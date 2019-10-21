1b248f5c3fe5142060060030a8ceb81be0ff0cfd
zeronedev <ok7375@gmail.com>
2019-10-21 23:20:26 +0900

    classList.toggle

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const hasClass = title.classList.contains(CLICKED_CLASS);
-
-  if (hasClass) {
-    title.classList.remove(CLICKED_CLASS);
-  } else {
-    title.classList.add(CLICKED_CLASS);
-  }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
